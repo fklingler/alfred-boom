e97b1e276ee9319140c27c26eae6a76fd572de39
Josiah Grace <josiahbgrace@gmail.com>
2014-10-29 17:33:08 -0500

    added support for ruby 2.0 in order for boom to work for yosemite

--- boom/boom.rb
+++ boom/boom.rb
@@ -1,5 +1,30 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby
+#!/System/Library/Frameworks/Ruby.framework/Versions/2.0/usr/bin/ruby
 # encoding: utf-8
+
+Encoding::default_external = Encoding::UTF_8 if defined? Encoding
+require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
+require File.join(File.dirname(__FILE__), 'bundle/bundler/setup')
+
+require 'boom'
+
+command = ARGV.shift
+
+if command == 'filter'
+  require './lib/boom/list'
+  require './lib/boom/item'
+  require './lib/boom/storage/base'
+
+  require 'alfred'
+  require './lib/alfred/feedback/item'
+  require './lib/alfred/feedback/file_item'
+
+  require './lib/boom/alfred'
+  require './lib/boom/alfred/filter'
+else
+  require './lib/boom/alfred/action'
+end
+
+Boom::Alfred.const_get(command.capitalize).execute(*ARGV)
 
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
 require File.join(File.dirname(__FILE__), 'bundle/bundler/setup')
