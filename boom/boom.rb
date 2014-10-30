7772c844b806b02c0d2665b579e982bef4e5b5e6
Josiah Grace <josiahbgrace@gmail.com>
2014-10-30 11:26:53 -0500

    replaced copy-paste mistake in boom.rb, update binary, still working

--- boom/boom.rb
+++ boom/boom.rb
@@ -22,27 +22,3 @@ else
 end
 
 Boom::Alfred.const_get(command.capitalize).execute(*ARGV)
-
-require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require File.join(File.dirname(__FILE__), 'bundle/bundler/setup')
-
-require 'boom'
-
-command = ARGV.shift
-
-if command == 'filter'
-  require './lib/boom/list'
-  require './lib/boom/item'
-  require './lib/boom/storage/base'
-
-  require 'alfred'
-  require './lib/alfred/feedback/item'
-  require './lib/alfred/feedback/file_item'
-
-  require './lib/boom/alfred'
-  require './lib/boom/alfred/filter'
-else
-  require './lib/boom/alfred/action'
-end
-
-Boom::Alfred.const_get(command.capitalize).execute(*ARGV)
